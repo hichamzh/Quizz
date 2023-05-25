f6339cac8cad375fc57fb7f2108e7c989a7eeae9
hichamzh <zouit.hicham1@gmail.com>
2023-05-25 13:51:18 +0200

    update nav bar

--- app/Composants/nav-bar.php
+++ app/Composants/nav-bar.php
@@ -12,22 +12,29 @@ if (session_status() === PHP_SESSION_ACTIVE) {
 <nav class="nav-bar">
     <img src="./public/Assets/logo_hfk.png" alt=""> 
     <ul class="nav-right">
-        <li> <a href="?controller=home&action=home">Accueil</a></li>
-        <?php if ( isset( $_SESSION['login']) ) : ?>
-            <li>
-                <a href='#regles'>Les Règles</a>
-                <form method="post" action="logout.php">
-                    <button type="submit">Déconnexion</button>
-                </form>
+        <li class="ligne">
+             <a 
+             <?php if(isset($_SESSION['login'])) :?>
+             href="?controller=home&action=acceuil"
+             <?php else :?>
+                href='?controller=home&action=home'
+                <?php endif ?>
+                >
+                Accueil
+            </a>
+        </li>
+        <?php if (isset( $_SESSION['login']) ) : ?>
+            <li class="ligne">
+                <a href='?controller=home&action=acceuil#regles'>Les Règles</a>
             </li>
             <form method="post" action="logout.php">
             <button type="submit" class="butt">Déconnexion</button>
             </form>
         <?php endif; ?>
 
-        <?php if (!isset( $_SESSION['login'])) : ?>
-            <li><a href='?controller=inscription&action=inscription' class='butt'>Sign Up</a></li>
-            <li><a href='?controller=inscription&action=connexion' class='butt'>Sign In</a></li>
+        <?php if (!isset($_SESSION['login'])) : ?>
+            <li class='butt'><a href='?controller=inscription&action=inscription' >Sign Up</a></li>
+            <li class='butt'><a href='?controller=inscription&action=connexion'>Sign In</a></li>
         <?php endif; ?>
     </ul>
 </nav>
