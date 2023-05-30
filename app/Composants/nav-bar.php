3dde055e8642f5e14fabdf46e74a556049791da3
fethi <benyuyu13@gmail.com>
2023-05-30 14:32:41 +0200

    OK30MAI_14h32

--- app/Composants/nav-bar.php
+++ app/Composants/nav-bar.php
@@ -12,29 +12,19 @@ if (session_status() === PHP_SESSION_ACTIVE) {
 <nav class="nav-bar">
     <img src="./public/Assets/logo_hfk.png" alt=""> 
     <ul class="nav-right">
-        <li class="ligne">
-             <a 
-             <?php if(isset($_SESSION['login'])) :?>
-             href="?controller=home&action=acceuil"
-             <?php else :?>
-                href='?controller=home&action=home'
-                <?php endif ?>
-                >
-                Accueil
-            </a>
-        </li>
-        <?php if (isset( $_SESSION['login']) ) : ?>
-            <li class="ligne">
-                <a href='?controller=home&action=acceuil#regles'>Les Règles</a>
+        <li> <a href="?controller=home&action=home">Accueil</a></li>
+        <?php if (isset($_SESSION['login'])) : ?>
+            <li>
+                <a href='#regles'>Les Règles</a>
             </li>
-            <form method="post" action="logout.php">
+            <form method="post" action="?controller=inscription&action=log_out">
             <button type="submit" class="butt">Déconnexion</button>
             </form>
         <?php endif; ?>
 
         <?php if (!isset($_SESSION['login'])) : ?>
-            <li class='butt'><a href='?controller=inscription&action=inscription' >Sign Up</a></li>
-            <li class='butt'><a href='?controller=inscription&action=connexion'>Sign In</a></li>
+            <li class='butt' ><a href='?controller=inscription&action=inscription'>S'inscrire</a></li>
+            <li class='butt' ><a href='?controller=inscription&action=connexion'>Se connecter</a></li>
         <?php endif; ?>
     </ul>
 </nav>
--- app/logout.php
+++ app/logout.php
@@ -3,12 +3,12 @@
 // session_start();
 
 // Détruisez toutes les variables de session
-// session_unset();
+session_unset();
 
 // Détruisez la session
 session_destroy();
 
 // Redirigez l'utilisateur vers une autre page (par exemple, la page d'accueil)
-header("Location: index.php");
+header("Location: index.php?controller=inscription&action=connexion");
 exit();
 ?>
