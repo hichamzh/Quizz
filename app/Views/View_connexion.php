209a3518db59c90194bd1c98a60cbf4db1145575
hichamzh <zouit.hicham1@gmail.com>
2023-06-12 21:15:58 +0200

    responsive

--- app/Views/View_connexion.php
+++ app/Views/View_connexion.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" />
     <link rel="stylesheet" href="public/Css/inscription.css">
     <title>Sign In</title>
 </head>
+
 <body>
     <div class="carre"></div>
     <div class="carre"></div>
@@ -24,22 +27,25 @@
     <?php if (isset($data)): ?>
         <script>alert("<?php echo $data['erreur'] ?>")</script>
     <?php endif ?>
-<header>
-        <h1>Se connecter</h1>
-    </header>
-    
+    <section class="connexion_inscription">
+
+        <header>
+            <h1>Se connecter</h1>
+        </header>
+
         <form action="?controller=inscription&action=connexion_verif" method="POST" class="formulaire-inscription">
             <div class="container-input">
-                <input type="text" id="login" placeholder="Identifiant..." name="login" required>   
+                <input type="text" id="login" placeholder="Identifiant..." name="login" required>
             </div>
             <div class="container-input">
                 <input type="password" id="mdp" placeholder="Mot de passe..." name="mdp">
                 <i class="fas fa-eye" onclick="toggleVisibleMdp()" id="icon"></i>
             </div>
-            <button type="submit" class="butt" >Go Quizz</button> <!--class butt heriter par la navbar -->
+            <button type="submit" class="butt">Go Quizz</button> <!--class butt heriter par la navbar -->
         </form>
     </section>
-    
+
     <script src="./public/Js/button_mdp.js" defer></script>
 </body>
+
 </html>
--- app/Views/View_inscription.php
+++ app/Views/View_inscription.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="public/Css/inscription.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" />
     <title>Inscription</title>
 </head>
+
 <body>
-    <?php if(isset($data['erreur'])) :?>
-    <script>alert("<?php echo $data['erreur']; ?>")</script>
+    <?php if (isset($data['erreur'])): ?>
+        <script>alert("<?php echo $data['erreur']; ?>")</script>
     <?php endif; ?>
     <div>
 
@@ -27,22 +30,25 @@
     <div class="carre"><img src="public/Assets/pikachu.png" alt="pikachu"></div>
     <div class="carre"></div>
     <div class="carre"></div>
-    <header>
-        <h1>Créer un compte Quizz</h1>
-    </header>
-    
+    <section class="connexion_inscription">
+        <header>
+            <h1>Créer un compte Quizz</h1>
+        </header>
+
         <form action="?controller=inscription&action=inscription_insert" method="POST" class="formulaire-inscription">
             <div class="container-input">
-                <input type="text" id="login" placeholder="Identifiant..." name="login" required minlength="5" maxlength="20">
+                <input type="text" id="login" placeholder="Identifiant..." name="login" required minlength="5"
+                    maxlength="20">
             </div>
             <div class="container-input">
                 <input type="password" id="mdp" placeholder="Mot de passe..." name="mdp" required minlength="8">
                 <i class="fas fa-eye" onclick="toggleVisibleMdp()" id="icon"></i>
             </div>
-            <button type="submit" class="butt" >S'inscrire</button> <!--class butt heriter par la navbar -->
+            <button type="submit" class="butt">S'inscrire</button> <!--class butt heriter par la navbar -->
         </form>
     </section>
-    
+
     <script src="./public/Js/button_mdp.js" defer></script>
 </body>
+
 </html>
