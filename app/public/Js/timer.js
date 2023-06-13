cedde5a950bbb96298470d5799871845419ded0f
hichamzh <zouit.hicham1@gmail.com>
2023-06-13 10:58:22 +0200

    maj timer

--- app/public/Js/timer.js
+++ app/public/Js/timer.js
@@ -3,14 +3,19 @@ timer.style.width = "100%";
 var timer_duration = 30;
 
 function updateTimer() {
-  var progressPercent = Math.floor((timer_duration / 30) * 100);
+  timer_duration--;
+  var progressPercent = Math.round((timer_duration / 30) * 100);
 
   console.log(progressPercent);
 
   timer.style.width = progressPercent + "%";
-  timer_duration--;
   // console.log(timer_duration);
-
+  if (progressPercent == 50) {
+    timer.style.backgroundColor = "orange";
+  }
+  if (progressPercent == 20) {
+    timer.style.backgroundColor = "red";
+  }
   if (timer_duration < 0) {
     //find du timer, on récup le form_qcm et on applique un submit
     document.querySelector(".form_qcm").submit();
