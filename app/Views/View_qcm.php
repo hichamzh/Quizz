10b6c03d7f14516b95a814be25378a6575f60a08
fethi <benyuyu13@gmail.com>
2023-06-15 14:21:09 +0200

    ballonfond

--- app/Views/View_qcm.php
+++ app/Views/View_qcm.php
@@ -11,44 +11,58 @@
 </head>
 
 <body>
+
+
+
+<!-- Generation d'image en fond selon le theme -->
+    <?php for ($i = 0; $i < 16; $i++) : ?>
+        <?php if ($_SESSION['id_theme'] == '7') : ?>
+            <div class="carre"></div>
+
+        <?php elseif ($_SESSION['id_theme'] == '8') : ?>
+            <div class="basket"></div>
+
+        <?php endif; ?>
+    <?php endfor; ?>
+
+<!-- Generation barre de timer -->
     <div class="progress_bar_container">
         <div class="progress_bar"></div>
     </div>
-
     <?php
     $score = $_SESSION['score'];
     $score_string = intval($score);
+
     ?>
 
     <form method="post" action="?controller=qcm&action=question_suivante" class="form_qcm">
         <input type="hidden" name="timer" value="0">
         <div class="qcm" class="larger-click-area">
-        <div class="qst_actuel">
-            Question N°
-            <?php
-            $compteur = $_SESSION['question_count'];
-            $compteur_string = intval($compteur) + 1;
-            echo $compteur_string;
-            ?>
-        </div>
+            <div class="qst_actuel">
+                Question N°
+                <?php
+                $compteur = $_SESSION['question_count'];
+                $compteur_string = intval($compteur) + 1;
+                echo $compteur_string;
+                ?>
+            </div>
             <h3>
                 <?= $data['question']->question; ?>
             </h3>
 
             <div class="container_reponses">
-            <?php foreach ($data['reponses'] as $r): ?>
+                <?php foreach ($data['reponses'] as $r) : ?>
 
                     <label for="reponse<?= $r->id_reponse ?>" class="larger-click-area centre">
-                        <input type="radio" id="reponse<?= $r->id_reponse ?>" name="reponse" class="margin "
-                            value="<?= $r->id_reponse ?>" required>
+                        <input type="radio" id="reponse<?= $r->id_reponse ?>" name="reponse" class="margin " value="<?= $r->id_reponse ?>" required>
                         <?= $r->reponse ?>
                     </label>
-                    
-                    <?php endforeach; ?>
+
+                <?php endforeach; ?>
             </div>
 
             <div class="button">
-                <input type="submit" value="Suivant" class="butt"/>
+                <input type="submit" value="Suivant" class="butt" />
             </div>
         </div>
     </form>
